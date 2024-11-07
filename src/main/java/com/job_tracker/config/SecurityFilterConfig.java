c335bb83feb3a6eebfc61b1d242e7edfdd4573f9
Shyam Shukla <shuklashyam279@gmail.com>
2024-11-07 15:31:39 +0530

    Configured CORS

--- src/main/java/com/job_tracker/config/SecurityFilterConfig.java
+++ src/main/java/com/job_tracker/config/SecurityFilterConfig.java
@@ -10,6 +10,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @AllArgsConstructor
@@ -22,17 +27,28 @@ public class SecurityFilterConfig {
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)
             throws Exception {
         httpSecurity
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable())
+                .cors(cors -> corsConfigurationSource())
                 .headers(header ->
                         header.frameOptions(frameOptions -> frameOptions.disable())
                 )
                 .authorizeHttpRequests(auth ->
                         auth
-                                .requestMatchers("/sign-up/", "/authenticate")
+                                .requestMatchers("/sign-up/", "/authenticate", "/")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
