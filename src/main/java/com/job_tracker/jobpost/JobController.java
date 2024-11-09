ece0975a2d4e2cab9b45de0478dd71c5ccea6fed
Shyam Shukla <shuklashyam279@gmail.com>
2024-11-09 18:53:35 +0530

    Implemented Map_Struct in JobPost Classes.

--- src/main/java/com/job_tracker/jobpost/JobController.java
+++ src/main/java/com/job_tracker/jobpost/JobController.java
@@ -16,12 +16,12 @@ import java.util.UUID;
 @RestController
 public class JobController {
     @Autowired
-    private JobPostService jobPostServices;
+    private JobPostServiceImpl jobPostServicesImpl;
 
     // =============================Retrieve All Job Posts==========================================
     @GetMapping("/v1/dashboard/all-jobs")
     public List<JobPostDTO> allJobPosts() {
-        return jobPostServices.allJobPosts();
+        return jobPostServicesImpl.allJobPosts();
     }
 
     // =============================Add User's Job Post==========================================
@@ -36,61 +36,61 @@ public class JobController {
                     .status(HttpStatus.BAD_REQUEST)
                     .body(errorMessage.toString());
         }
-        return jobPostServices.createJobPosts(jobPost);
+        return jobPostServicesImpl.createJobPosts(jobPost);
     }
 
     // =============================Set Resume to User's Job Post==========================================
     @PutMapping("/v1/set-resume")
     public ResponseEntity<JobPostDTO> setResume(@RequestBody JobPost jobPost, @RequestParam UUID resumeID) {
-        return jobPostServices.setResume(jobPost, resumeID);
+        return jobPostServicesImpl.setResume(jobPost, resumeID);
     }
 
     // ================================Delete User's Job Post=======================================
     @DeleteMapping("/v1/delete-job-post")
     public ResponseEntity<String> deleteUsersJobPost(@RequestParam UUID jobPostId) {
-        return jobPostServices.deleteUsersJobPost(jobPostId);
+        return jobPostServicesImpl.deleteUsersJobPost(jobPostId);
     }
 
     // ===============================Retrieve User's Job Posts========================================
     @GetMapping("/v1/retrieve-user-job-posts")
     public ResponseEntity<List<JobPostDTO>> retrieveUserJobPosts() {
-        return jobPostServices.retrieveUserJobPosts();
+        return jobPostServicesImpl.retrieveUserJobPosts();
     }
 
     // ============================Count User's Job Posts=============================================
     @GetMapping("/v1/count-user-job-posts")
     public ResponseEntity<Integer> countUserJobPosts(){
-        return jobPostServices.countUserJobPosts();
+        return jobPostServicesImpl.countUserJobPosts();
     }
 
     // ============================Retrieve User's Job Post with Job-post Id==========================
     @GetMapping("/v1/retrieve-job-post-with-user-id")
     public JobPost retrieveJobPostWithUserId(UUID jobPostId){
-        return jobPostServices.retrieveUserJobPostWithId(jobPostId);
+        return jobPostServicesImpl.retrieveUserJobPostWithId(jobPostId);
     }
 
     // ============================Update Job Post Details============================================
     @PutMapping("/v1/update-job-post")
     public ResponseEntity<String> updateJobPost(@RequestBody JobPost jobPost){
-        return jobPostServices.updateJobPost(jobPost);
+        return jobPostServicesImpl.updateJobPost(jobPost);
     }
 
     // ========================Add Job Posts From Dashboard to User's Account=========================
     @PostMapping("/v1/add-job-with-job-id")
     public ResponseEntity<String> addJobWithJobId(@RequestParam UUID jobPostId){
-        return jobPostServices.addJobWithJobId(jobPostId);
+        return jobPostServicesImpl.addJobWithJobId(jobPostId);
     }
 
     // =========================Retrieve User's Job Posts Count Of Each Day==============================
     @GetMapping("/v1/dashboard/retrieve-jobpost-count-per-day")
     public List<Object[]> retrieveUsersPerDayJobPosts() {
-        return jobPostServices.retrieveUsersPerDayJobPosts();
+        return jobPostServicesImpl.retrieveUsersPerDayJobPosts();
     }
 
     // ====================================Top 3 Performer's of the day with their Job Counts ===========================
     @GetMapping("/v1/dashboard/top-three-performer-of-the-day-with-their-job-count")
     public ResponseEntity<List<TopPerformerDTO>> topPerformersOfTheDay() {
-        return jobPostServices.retrieveTopPerformersOfTheDay();
+        return jobPostServicesImpl.retrieveTopPerformersOfTheDay();
     }
 
     // =============================Retrieve JobPosts with Filters Applied===================================
@@ -102,7 +102,7 @@ public class JobController {
             @RequestParam LocalDate jobDate,
             @RequestParam JobStatusEnum status
     ) {
-        return jobPostServices.retrieveJobsByFilters(
+        return jobPostServicesImpl.retrieveJobsByFilters(
                 jobTitle,
                 companyName,
                 jobDescription,
@@ -114,18 +114,18 @@ public class JobController {
     // ========================================Dashboard Search Functionality=============================================
     @GetMapping("/v1/dashboard/search-jobposts-containing-strings")
     public ResponseEntity<List<JobPostDTO>> retrieveJobPostsWithString(@RequestParam String string){
-        return jobPostServices.retrieveJobPostsContainingString(string);
+        return jobPostServicesImpl.retrieveJobPostsContainingString(string);
     }
 
     // ======================================Search Box For User Component===================================
     @GetMapping("/v1/search-user-jobposts-containing-string")
     public ResponseEntity<List<JobPostDTO>> retrieveUserJobPostsContainingString(@RequestParam String string) {
-        return jobPostServices.retrieveUserJobPostsContainingString(string);
+        return jobPostServicesImpl.retrieveUserJobPostsContainingString(string);
     }
 
     // =======================================Retrieve Job Post Count Per Day================================
     @GetMapping("/v1/retrieve-jobpost-count-per-day")
     public ResponseEntity<List<Object[]>> retrieveJobCountsPerDay() {
-        return jobPostServices.retrieveJobCountsPerDay();
+        return jobPostServicesImpl.retrieveJobCountsPerDay();
     }
 }
