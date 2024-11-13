aa94b2245489443d9b21285f64d24890de1d67ea
Shyam Shukla <shuklashyam279@gmail.com>
2024-11-14 04:44:05 +0530

    Learned about testing secure endpoints, Tested Welcome API

--- src/test/java/com/job_tracker/home/HomePageTest.java
+++ src/test/java/com/job_tracker/home/HomePageTest.java
@@ -1,58 +1,44 @@
 package com.job_tracker.home;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.job_tracker.jobpost.JobPostService;
-import com.job_tracker.jwtAuthentication.JwtResponse;
-import com.job_tracker.user.UserService;
-import org.junit.jupiter.api.BeforeEach;
+import com.job_tracker.jwtAuthentication.JwtHelper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
-import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
-@WebMvcTest
 @AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(controllers = HomePage.class)
 @ExtendWith(MockitoExtension.class)
-@Import(SecurityConfig.class)
 public class HomePageTest {
 
     @MockBean
-    private JwtResponse jwtResponse;
+    private JwtHelper jwtHelper;
+
+    @MockBean
+    private UserDetailsService userDetailsService;
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private UserService userService;
-
-    @MockBean
-    private JobPostService jobPostService;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    private HomePage homePage;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
     @Test
-    @WithMockUser("user")
+    @WithMockUser(roles = "user")
     void testWelcome() throws Exception {
-        assertThat(homePage).isNotNull();
-
+        mockMvc
+                .perform(
+                        MockMvcRequestBuilders
+                                .get("/")
+                                .with(SecurityMockMvcRequestPostProcessors.jwt())
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
